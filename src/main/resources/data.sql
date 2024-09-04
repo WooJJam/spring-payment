6f3a2f9c9cda75e5ac70ab012efa96b4b954b7fa
WooJJam <111514410+WooJJam@users.noreply.github.com>
2024-09-04 15:05:09 +0900

    chore: data.sql 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO payment (name, price, amount) VALUES
+                                              ('Product A', 100, 2),
+                                              ('Product B', 200, 5)
+#                                               ('Product C', 1500, 1),
+#                                               ('Product D', 3000, 3),
+#                                               ('Product E', 2500, 4);
